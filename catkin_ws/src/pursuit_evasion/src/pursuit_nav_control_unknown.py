a20a1fe5348a68bf470223d507e5b82c16f4de8e
lunaab <lunaab0131@gmail.com>
2018-12-14 13:49:21 -0500

    D

--- catkin_ws/src/pursuit_evasion/src/pursuit_nav_control_unknown.py
+++ catkin_ws/src/pursuit_evasion/src/pursuit_nav_control_unknown.py
@@ -84,7 +84,7 @@ class PursuitNavControl(object):
             self.path.reverse()
         
     def setPose(self, pose_msg):
-        ''''if self.curr_pose is not None:
+        '''if self.curr_pose is not None:
             if pose_msg.data[0] != self.curr_pose[1]:
                 pass
             else:
@@ -93,7 +93,7 @@ class PursuitNavControl(object):
             self.curr_pose = (pose_msg.data[0], pose_msg.data[1])
         msg = Int16MultiArray()
         msg.data = [self.curr_pose[0], self.curr_pose[1], 0]
-        self.pub_pose.publish(msg)''''
+        self.pub_pose.publish(msg)'''
         if self.curr_pose is None:
             self.curr_pose = (pose_msg.data[0], pose_msg.data[1])
         rospy.logwarn(self.curr_pose)
